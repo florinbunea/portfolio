f5d93c0d5afdb4ffcda3fc9b93ec3a8c7b676478
Florin Bunea <danflorinbunea@Alecs-MBP-3.localdomain>
2019-05-03 12:03:37 +0100

    Updates

---
+++ precache-manifest.86e514bdcd22a324e0575cbaeca6727e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf93d7c7619c57e8ad1c112fbb06869",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d2c490ef90df1d592d88",
+    "url": "/react-gh-pages/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-gh-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d2c490ef90df1d592d88",
+    "url": "/react-gh-pages/static/js/main.f883b0db.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
