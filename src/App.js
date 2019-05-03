c65d038823627c72eceace732e46e28d88bbdda9
Florin Bunea <danflorinbunea@Alecs-MBP-3.localdomain>
2019-05-03 12:26:36 +0100

    try commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Florin's Portfolio <code>src/App.js</code> and save to reload.
+        Florin's Portfolio !
         </p>
         <a
           className="App-link"
