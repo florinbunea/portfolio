b9eba579aee2b2b3a3800ab8566fa269e1ff56ce
Florin Bunea <danflorinbunea@Alecs-MBP-3.localdomain>
2019-05-07 16:15:38 +0100

    updates and create components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import image from './image/background.jpg';
+import image from './image/journey.jpeg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/Main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={image} className="logo" alt="logo" />
-        <p>
-        Florin Bunea Portfolio !
-        </p>
-      </header>
+    <div className="demo-big-content">
+      <Layout>
+        <Header title="Title" scroll>
+          <Navigation>
+            <Link to="/reume">Resume</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <Link to="/reume">Resume</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
     </div>
-  );
+  )
 }
 
 export default App;
